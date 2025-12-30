2e7ea9c8eac47c7c4d902a82ebcfd3e729707817
MAROUANE MORAD <moradmarouane13@gmail.com>
2025-12-30 14:32:56 +0100

    test

--- LMSConnector/jest.config.js
+++ LMSConnector/jest.config.js
@@ -1,5 +1,9 @@
 export default {
   testEnvironment: "node",
   roots: ["<rootDir>/tests"],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  // ...existing code...
   // extensionsToTreatAsEsm: ['.js'],
 };
